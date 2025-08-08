019424b1834a2ef32d7224d35b87c862d3fbffb4
Samarth <samarthpatil2021.comp@mmcoe.edu.in>
2025-08-08 23:15:17 +0530

    added scroll

--- fr/src/components/Hero.jsx
+++ fr/src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from './Logo'
 import YoutubeSvg from '../utils/YoutubeSvg'
 import InstagramSvg from '../utils/InstagramSvg'
+import { motion } from 'motion/react'
 function Hero() {
   return (
     <>
@@ -21,6 +22,19 @@ function Hero() {
       {/* Foreground Content */}
       <div className="flex flex-col items-center justify-center h-full text-white text-center px-4">
         <Logo isSmall={false}></Logo>
+        <div className="border-2 w-5 h-10 rounded-3xl m-2 flex justify-center">
+          <motion.div 
+          className="w-2 h-2 bg-white rounded-full m-1"
+          animate = {{
+            y : [ 0 , 20, 0]
+          }}
+          transition={{
+            duration: 1.2,
+            repeat: Infinity,
+            ease: "easeInOut"
+          }}
+          ></motion.div>
+        </div>
         <button className="px-6 py-2 border border-white text-white bg-transparent hover:bg-white hover:text-black transition duration-300 hover:cursor-pointer">
           WORK WITH US
         </button>
