3d7a12130afb912e480ada4a4cd0f3c805994970
Samarth <samarthpatil2021.comp@mmcoe.edu.in>
2025-08-10 14:13:59 +0530

    dont with the Members card (Need to add the socials)

--- fr/src/components/Body.jsx
+++ fr/src/components/Body.jsx
@@ -7,41 +7,24 @@ import MemberCard from './MemberCard'
 function Body() {
   return (
     <>
-      <div className='bg-[#161716] h-full p-5 flex '>
-        <div className="relative w-64 h-64">
-          {/* Background Image */}
-          <img
-            src="/bg-for-logo.jpeg"
-            alt="Background"
-            className="w-full h-full rounded-3xl"
-          />
-
-          {/* Overlay Image */}
-          <img
-            src="/logo.png"
-            alt="logo"
-            className="absolute top-20 left-20 w-24 h-24 scale-150"
-          />
-        </div>
-
+      <div className='bg-[#161716] h-full p-5 flex '>   
         <div className=' border-[1px] border-amber-50 rounded-2xl bg-[#1B1A1B] w-[350px] h-64 ml-9 flex justify-center items-center sele'>
           <p className='text-white text-xl font-sans'>
             A group of friends with a few <br/>videos online, over million fans <br/> and half a billion of views
           </p>
         </div>
-        <div className='flex-row'>
-          <div className='w-[800px] ml-9'>
-            <img src="/group.jpg" alt="group img" className='rounded-2xl '/>
-            
+        {/* <div className='flex-row'> */}
+          <div className='w-[1300px] h-64 ml-5'>
+            <img src="/group.jpg" alt="group img" className='rounded-2xl h-full '/>
           </div>
 
-          <div className='w-[800px] h-[100px] rounded-2xl bg-black ml-9 mt-3 flex items-center justify-center'>
+          {/* <div className='w-[800px] h-[100px] rounded-2xl bg-black ml-9 mt-3 flex items-center justify-center'>
             <div className='text-white flex justify-between'>
               <YoutubeSvg/>
               <InstagramSvg/>
             </div>
-          </div> 
-        </div>
+          </div>  */}
+        {/* </div> */}
         
       </div>
       <div className='flex justify-center items-center bg-[#161716]'>
@@ -49,12 +32,16 @@ function Body() {
       </div>
 
       <div className="grid grid-cols-3 gap-4 bg-[#161716] p-8 pt-20">
-        <MemberCard imgSrc={'/Divu.jpg'}/>
-        <MemberCard imgSrc={'/marka.webp'}/>
-        <MemberCard imgSrc={'/yjr.jpg'}/>
-        <MemberCard imgSrc={'/chonky.jpg'}/>
-        <MemberCard imgSrc={'/mohak 1.webp'}/>
-        <MemberCard imgSrc={'/all players.jpg'}/>
+        <MemberCard imgSrc={'/Divu.jpg'} MemberName={"Divyansh"} MemberYT={"https://www.youtube.com/@DivyanshCR7"} MemberInfo={"It’s DivyanshCR7 — your go-to for calm, clear football talk. I keep it cool, even when Ronaldo scores a hat-trick… okay, maybe not that calm"}/>
+        <MemberCard imgSrc={'/marka.webp'} MemberName={"Markaroni"} MemberYT={"https://www.youtube.com/@Markaroni"} MemberInfo={"Markaroni in the house — serving football takes hot, Barça runs in my veins and sarcasm fuels my uploads and I love Football but MESSI more."}/>
+        <MemberCard imgSrc={'/yjr.jpg'} MemberName={"YjR"} MemberYT={"https://www.youtube.com/@yashyjr"} MemberInfo={"YjR here — I bleed Madridismo. Transfers, goals, and why Viní Jr. is basically football’s cheat code! okay may be not for last season — that’s my daily grind."}/>
+        <MemberCard imgSrc={'/chonky.jpg'} MemberName={"Chonkyy"} MemberYT={"https://www.youtube.com/@chonkyy7871"} MemberInfo={"I’m Chonky — welcome to my football tent, where the takes are hot, the banter’s loud, and your club’s defense probably isn’t safe."}/>
+        <MemberCard imgSrc={'/mohak 1.webp'} MemberName={"Mohak"} MemberYT={"https://www.youtube.com/@ONEMUFC/videos"} MemberInfo={"It’s Mohak here — your daily dose of Manchester United talkserving football takes hotter than Maguire’s head after a mistake. From Indian football to the Premier League, nothing’s safe from my sarcasm."}/>
+        <MemberCard imgSrc={'/all players.jpg'} MemberName={"The OG's"} MemberYT={"https://www.youtube.com/@ChaosClub.Official"} MemberInfo={""}/>
+      </div>
+
+      <div className=''>
+
       </div>
     </>
   )
--- fr/src/components/MemberCard.jsx
+++ fr/src/components/MemberCard.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion  , AnimatePresence } from "motion/react"
 
-function MemberCard({ imgSrc }) {
+function MemberCard({ imgSrc , MemberName ,MemberYT , MemberInfo}) {
+
+  const [hover, setHover] = useState(false);
+
+
   return (<>
 
-<div className="h-[450px] w-[460px]">
+<div className="relative h-[450px] w-[460px] group" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
   <img
     src={imgSrc}
     alt="img1"
     className="h-full w-full rounded-lg object-cover"
   />    
+
+  <AnimatePresence>
+    {/* Show overlay only when hovered */}
+    {hover && (
+      <motion.div
+          initial={{ opacity: 0    }} // start small & transparent
+          animate={{ opacity: 0.85, scale: 1 }} // grow to full size & visible
+          exit={{ opacity: 0 }} // shrink & fade out
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+          className="absolute top-0 left-0 w-full h-full bg-[#1544D7] rounded-lg flex-col "
+        > 
+          <div className='mt-20 w-64 ml-2'>
+            <p className='text-white font-sans text-[17px]'>
+              {MemberInfo}
+            </p>
+          </div>
+          <div className='mb-4 mt-40'>
+            <div className=" absolute top-96 bg-black opacity-100 text-white text-xl w-fit p-1 cursor-pointer">
+              <p onClick={() => window.open(MemberYT, '_blank')}
+              >Meet {MemberName}</p>
+            </div>
+          </div>
+          
+      </motion.div>
+    )}
+  </AnimatePresence>
 </div>
+
+
 
 
 </>)}
